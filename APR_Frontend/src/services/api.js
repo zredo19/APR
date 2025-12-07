eedf09c29f47cdf055c626529ab025e62a3d490c
Rodolfo Obreque <rodolfoobreque19@gmail.com>
2025-12-07 19:13:31 -0300

    Se agregan weas para subirlo a render

--- APR_Frontend/src/services/api.js
+++ APR_Frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Creamos una instancia de Axios con la URL base de tu Backend
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
   headers: {
     'Content-Type': 'application/json',
   },
