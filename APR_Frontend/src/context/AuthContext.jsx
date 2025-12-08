8ca0263595ea6c40e004aaa421e05790311f45b2
Rodolfo Obreque <rodolfoobreque19@gmail.com>
2025-12-07 21:37:11 -0300

    fix: url login hardcodeada

--- APR_Frontend/src/context/AuthContext.jsx
+++ APR_Frontend/src/context/AuthContext.jsx
@@ -31,7 +31,11 @@ export const AuthProvider = ({ children }) => {
 
             const API_URL = import.meta.env.VITE_API_URL;
 
-            const response = await axios.post(`${API_URL}/token`, formData)
+            const response = await axios.post(`${API_URL}/token`, formData, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            });
 
             const { access_token } = response.data;
 
